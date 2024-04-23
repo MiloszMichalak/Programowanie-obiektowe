ba3169357fdf882f3db0bb43bee6698c6878680a
miloszmichalak <miloszmichalak150@gmail.com>
2024-04-23 12:16:48 +0200

    Dodanie lekcji 23.04 - dziedziczenie

---
+++ X.04.24/23.04/classes/Vehicle.cs
@@ -0,0 +1,49 @@
+namespace _23._04.classes;
+
+public enum FuelType
+{
+    Petrol,
+    Diesel,
+    Electric
+}
+
+public class Vehicle
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    private double Fuel{ get; set; }
+    public FuelType FuelType { get; protected set; }
+    public ushort Speed { get; set; }
+
+    public Vehicle(string brand, string model, ushort speed, FuelType fuelType)
+    {
+        Brand = brand;
+        Model = model;
+        Speed = speed;
+        FuelType = fuelType;
+        Fuel = 5;
+    }
+
+    public virtual string GetInfo()
+    {
+        return $"Pojazd {Brand} {Model} \n" +
+               $"Predkosc maksymalna: {Speed} \n" +
+               $"Rodzaj paliwa: {FuelType}";
+    }
+
+    public virtual string StartEngine()
+    {
+        return "Engine started";
+    }
+
+    public virtual string StopEngine()
+    {
+        return "Engine stopped";
+    }
+
+    public void Refuel(double amount)
+    {
+        Fuel += amount;
+        Console.WriteLine($"Zatankowano {amount} litrow paliwa w pojezdzie ");
+    }
+}

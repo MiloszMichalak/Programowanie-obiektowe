b07bd280240a8bb90498fc84a4496edc02a1b1c9
miloszmichalak <miloszmichalak150@gmail.com>
2024-09-09 15:48:46 +0200

    Dodanie zadania z lekcji

---
+++ Klasa 3/9.IX/dziedziczenie/classes/Circle.cs
@@ -0,0 +1,26 @@
+namespace dziedziczenie.classes;
+
+public class Circle : Shape
+{
+    private float radius;
+
+    public Circle(float radius)
+    {
+        this.radius = radius;
+    }
+
+    public void SetRadius(float radius)
+    {
+        this.radius = radius;
+    }
+
+    public override float CalculateArea()
+    {
+        return (float)Math.Round(Math.PI * radius * radius, 2);
+    }
+
+    public override float CalculatePerimeter()
+    {
+        return (float)Math.Round(2 * Math.PI * radius, 2);
+    }
+}
---
+++ Klasa 3/9.IX/dziedziczenie/classes/Shape.cs
@@ -0,0 +1,14 @@
+namespace dziedziczenie.classes;
+
+public class Shape
+{
+    public virtual float CalculateArea()
+    {
+        return 0.0f;
+    }
+    
+    public virtual float CalculatePerimeter()
+    {
+        return 0.0f;
+    }
+}

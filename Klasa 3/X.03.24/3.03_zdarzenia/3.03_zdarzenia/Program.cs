fee01350f1eacc48ae6a886c718c137b1a35c324
MiloszMichalak <miloszmichalak150@gmail.com>
2025-03-03 22:43:24 +0100

    Dodano zdarzenia

---
+++ Klasa 3/X.03.24/3.03_zdarzenia/3.03_zdarzenia/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _3._03_zdarzenia;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PasswordManager.PasswordVerified += (username, success) => 
+            Console.WriteLine($"Logownie uzytkownika o nazwie {username}: {(success ? "udane" : "nieudane")}");
+        
+        PasswordManager.SavePassword("admin", "admin");
+        Console.WriteLine("Wprowadz nazwe uzytkownika: ");
+    }
+}

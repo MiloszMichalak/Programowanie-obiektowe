d2899dc93ca2246b7cdc1a4a0e89d45a395214b3
miloszmichalak <miloszmichalak150@gmail.com>
2026-03-19 11:08:24 +0100

    Dodanie zadan z Dependency Inverion Principle

---
+++ Klasa 4/X.03.26/DIPZadanie/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace DIPZadanie;
+
+public interface IChargingMethod
+{
+    void Charge();
+}
+
+public class AcCharging : IChargingMethod
+{
+    public void Charge()
+    {
+        Console.WriteLine("Ładowanie AC");
+    }
+}
+
+public class DcCharging : IChargingMethod
+{
+    public void Charge()
+    {
+        Console.WriteLine("Ładowanie DC");
+    }
+}
+
+public class InductiveCharging : IChargingMethod
+{
+    public void Charge()
+    {
+        Console.WriteLine("Ładowanie indukcyjne");
+    }
+}
+
+public class SolarCharging : IChargingMethod
+{
+    public void Charge()
+    {
+        Console.WriteLine("Ładowanie solarne");
+    }
+}
+
+public class CarCharger(IChargingMethod chargingMethod)
+{
+    public void StartCharging()
+    {
+        Console.WriteLine("Rozpoczynam ładowanie pojazdu");
+        chargingMethod.Charge();
+        Console.WriteLine("Ładowanie zakończone.\n");
+    }
+}
+
+static class Program
+{
+    static void Main()
+    {
+        var charger = new CarCharger(new DcCharging());
+        charger.StartCharging();
+
+        var solarCharger = new CarCharger(new SolarCharging());
+        solarCharger.StartCharging();
+    }
+}
